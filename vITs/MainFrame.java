293fc34f8bb27a4ac9864b7463b2e3f4c6a7c7b4
psaxin <pjar_saxin@hotmail.com>
2015-04-08 12:39:26 +0200

    Uppdaterat GUI

--- vITs/MainFrame.java
+++ vITs/MainFrame.java
@@ -6,6 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import java.awt.Color;
+import java.awt.Panel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 public class MainFrame extends JFrame {
 
@@ -30,14 +39,30 @@ public class MainFrame extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * test
 	 */
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 549, 372);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(5, 147, 508, 165);
+		panel.add(tabbedPane);
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("New tab", null, tabbedPane_1, null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Pierre\\git\\vITsAlpha\\vITs\\resource\\vITsLogga.gif"));
+		lblNewLabel.setBounds(5, 0, 508, 147);
+		panel.add(lblNewLabel);
 	}
-
 }
