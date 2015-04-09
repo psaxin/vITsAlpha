c03d2bd46f9bd029828ce99ee9a281f3ed271b25
Klas Tärnström <klas@tarnstrom.se>
2015-04-09 15:40:21 +0200

    Update MainFrame.java

--- vITs/MainFrame.java
+++ vITs/MainFrame.java
@@ -1,32 +1,17 @@
 package vITs;
 
-import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTabbedPane;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.FlowLayout;
-import java.awt.Color;
-import java.awt.Panel;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import javax.swing.JTextField;
+import java.awt.*;
+import javax.swing.*;
 
 public class MainFrame extends JFrame {
 
-	private DBConnect con;
-	private JPanel mainPane;
-	private JTextField textField;
-	private JTextField textField_1;
-
 	/**
-	 * Launch the application.
-	 * HEJ JOHAN
+	 * 
+	 * @param args
+	 * test12
 	 * test12
 	 */
 	public static void main(String[] args) {
@@ -47,63 +32,23 @@ public class MainFrame extends JFrame {
 	 * test
 	 */
 	public MainFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 591, 467);
-		mainPane = new JPanel();
-		mainPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(mainPane);
-		mainPane.setLayout(null);
-		
-		JPanel mainPanel = new JPanel();
-		mainPanel.setBounds(5, 5, 560, 412);
-		mainPane.add(mainPanel);
-		mainPanel.setLayout(null);
-		
-		JTabbedPane mainTabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		mainTabbedPane.setBounds(5, 147, 545, 265);
-		mainPanel.add(mainTabbedPane);
-		
-		JTabbedPane reportTP = new JTabbedPane(JTabbedPane.TOP);
-		mainTabbedPane.addTab("Rapportering", null, reportTP, null);
-		
-		JPanel newReportPanel = new JPanel();
-		reportTP.addTab("Skapa ny", null, newReportPanel, null);
-		newReportPanel.setLayout(null);
-		
-		textField = new JTextField();
-		textField.setBounds(10, 11, 86, 20);
-		newReportPanel.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(10, 42, 86, 20);
-		newReportPanel.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JPanel changeReportPanel = new JPanel();
-		reportTP.addTab("Komplettera", null, changeReportPanel, null);
-		
-		JPanel manageReportsPanel = new JPanel();
-		mainTabbedPane.addTab("Hantera rapporter", null, manageReportsPanel, null);
-		
-		JPanel applyTravelExpensesPanel = new JPanel();
-		mainTabbedPane.addTab("S\u00F6k reseers\u00E4ttning", null, applyTravelExpensesPanel, null);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon(MainFrame.class.getResource("/vITs/resource/vITsLogga.gif")));
-		lblNewLabel.setBounds(5, 0, 508, 147);
-		mainPanel.add(lblNewLabel);
-		
+	
+        setDefaultCloseOperation(3);
+        setTitle("vITs Resehantering");
+        setPreferredSize(new Dimension(640, 480));
+        setResizable(false);
+       
+        Logo logo = new Logo();
+        add(logo);
+        
+        Tabs tabs = new Tabs();
+        add(tabs);
+
+        pack();
 		
 		/*
-		 * KOD SOM INTE ÄR SWING
+		 * KOD SOM INTE Ã„R SWING
 		 */
 	
-		con = new DBConnect();
-		try {
-		con.readDB("SELECT * FROM rapport");
-		} catch (Exception e) {
-			
-		}
 	}
 }
